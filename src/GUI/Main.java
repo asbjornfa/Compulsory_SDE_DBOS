a5f61b7cf0bd786ceb6aef96f6097b96ce9fb6d7
nikolaj1307 <nikolaj1307@live.dk>
2024-03-19 21:04:35 +0100

    JavaFX project update
    - Created necessary files, to run the program.

---
+++ src/GUI/Main.java
@@ -0,0 +1,22 @@
+package GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        Application.launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/View/SlideshowView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        primaryStage.setTitle("Image viewer");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
