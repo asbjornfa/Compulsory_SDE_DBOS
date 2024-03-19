b3bceaceddc5c38737b89b4b21e12978837faf99
nikolaj1307 <nikolaj1307@live.dk>
2024-03-20 00:21:01 +0100

    Fxml update
    Added fx id's & it's now possible to go between the SlideshowView & ImagesView.

--- src/GUI/Controllers/ImagesViewController.java
+++ src/GUI/Controllers/ImagesViewController.java
@@ -1,4 +1,78 @@
 package GUI.Controllers;
 
-public class ImagesViewController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ImagesViewController implements Initializable{
+
+    private SlideshowViewController slideshowViewController;
+
+    // Setter for controller
+    public void setSlideshowViewController(SlideshowViewController slideshowViewController) {
+        this.slideshowViewController = slideshowViewController;
+    }
+
+    @FXML
+    private Button addFilesBtn;
+
+    @FXML
+    private Button deleteBtn;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private Button loadImagesBtn;
+
+    @FXML
+    private MenuItem menuImages;
+
+    @FXML
+    private MenuItem menuSlideshow;
+
+    @FXML
+    private TableView<?> tblView;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //Midlertidig løsning for at vise billede i ImageView..
+        Image image = new Image("/Images/nature.jpg");
+        imageView.setImage(image);
+    }
+
+    @FXML
+    public void onClickMenuSlideshow(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/SlideshowView.fxml"));
+        Parent root = loader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Image Viewer");
+        stage.show();
+
+        // Get the controller for the AddMovieView.fxml
+        SlideshowViewController slideshowViewController1 = loader.getController();
+        // Pass the reference to the main controller to allow communication between controllers
+        slideshowViewController.setImagesViewController(this);
+    }
+
+
+    public void onClickMenuImages(ActionEvent event) {
+    }
+
+
 }
--- src/GUI/Controllers/SlideshowViewController.java
+++ src/GUI/Controllers/SlideshowViewController.java
@@ -1,4 +1,102 @@
 package GUI.Controllers;
 
-public class SlideshowViewController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SlideshowViewController implements Initializable {
+
+    private ImagesViewController imagesViewController;
+
+    //Defines a Stage reference
+    private Stage slideshowStage;
+
+    // Setter for controller
+    public void setImagesViewController(ImagesViewController imagesViewController) {
+        this.imagesViewController = imagesViewController;
+    }
+
+    @FXML
+    private Label blueLbl;
+
+    @FXML
+    private Button clearBtn;
+
+    @FXML
+    private Label currentlyLbl;
+
+    @FXML
+    private Label greenLbl;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private Button leftSkipBtn;
+
+    @FXML
+    private Button loadImagesBtn;
+
+    @FXML
+    private MenuItem menuImages;
+
+    @FXML
+    private MenuItem menuSlideshow;
+
+    @FXML
+    private Button playPauseBtn;
+
+    @FXML
+    private Label redLbl;
+
+    @FXML
+    private Button rightSkipBtn;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //Midlertidig løsning for at vise billede i ImageView..
+        Image image = new Image("/Images/nature.jpg");
+        imageView.setImage(image);
+    }
+
+    @FXML
+    public void onClickMenuImages(ActionEvent event) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/ImagesView.fxml"));
+        Parent root = loader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Image Viewer");
+        stage.show();
+
+        // Get the controller for the AddMovieView.fxml
+        ImagesViewController imagesViewController = loader.getController();
+        // Pass the reference to the main controller to allow communication between controllers
+        imagesViewController.setSlideshowViewController(this);
+
+
+
+    }
+
+
+
+    @FXML
+    public void onClickMenuSlideshow(ActionEvent event) {
+
+    }
+
+
 }
